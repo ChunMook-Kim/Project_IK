f1b42cca5a682cd727aaf2d3a7eb46a675961d61
Sinil <rtd99062@gmail.com>
2024-09-19 23:42:23 +0900

    #3 - Established a file IO system using game instance
    IK_Proto_GameData.csv - CSV file for game data.
    IK_Proto_GameData.uasset - UE imported game data blueprint class. UE handles it.
    CharacterStatComponent.cpp&.h - Changed its base class to ActorComponent class. Added getters&setters of its member variables.
    IKGameInstance.cpp&.h - Created a struct to contain game file data. GameInstance now import game file data when the game app initialized. It allows us to query game character data using a function named GetCharacterData(char_id).

--- Source/Project_IK/Private/CharacterStatComponent.cpp
+++ Source/Project_IK/Private/CharacterStatComponent.cpp
@@ -11,24 +11,104 @@ See LICENSE file in the project root for full license information.
 #include "CharacterStatComponent.h"
 
 // Sets default values
-ACharacterStatComponent::ACharacterStatComponent()
+UCharacterStatComponent::UCharacterStatComponent()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
+	bWantsInitializeComponent = true;
 }
 
 // Called when the game starts or when spawned
-void ACharacterStatComponent::BeginPlay()
+void UCharacterStatComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void ACharacterStatComponent::Tick(float DeltaTime)
+void UCharacterStatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunctionoverride)
 {
-	Super::Tick(DeltaTime);
+	Super::TickComponent(DELTA, TickType, ThisTickFunctionoverride);
+}
 
+float UCharacterStatComponent::GetAbilityPower() noexcept
+{
+	return ability_power_;
+}
+
+float UCharacterStatComponent::GetAttack() noexcept
+{
+	return attack_;
+}
+
+float UCharacterStatComponent::GetAttackSpeed() noexcept
+{
+	return attack_speed_;
+}
+
+float UCharacterStatComponent::GetHitPoint() noexcept
+{
+	return hit_point_;
+}
+
+float UCharacterStatComponent::GetMagazine() noexcept
+{
+	return magazine_;
+}
+
+float UCharacterStatComponent::GetFireRange() noexcept
+{
+	return fire_range_;
+}
+
+float UCharacterStatComponent::GetMoveSpeed() noexcept
+{
+	return move_speed_;
+}
+
+float UCharacterStatComponent::GetSightRange() noexcept
+{
+	return sight_range_;
+}
+
+void UCharacterStatComponent::SetAbilityPower(float ability_power) noexcept
+{
+	ability_power_ = ability_power;
+}
+
+void UCharacterStatComponent::SetAttack(float attack) noexcept
+{
+	attack_ = attack;
+}
+
+void UCharacterStatComponent::SetAttackSpeed(float attack_speed) noexcept
+{
+	attack_speed_ = attack_speed;
+}
+
+void UCharacterStatComponent::SetHitPoint(float hit_point) noexcept
+{
+	hit_point_ = hit_point;
+}
+
+void UCharacterStatComponent::SetMagazine(float magazine) noexcept
+{
+	magazine_ = magazine;
+}
+
+void UCharacterStatComponent::SetFireRange(float fire_range) noexcept
+{
+	fire_range_ = fire_range;
+}
+
+void UCharacterStatComponent::SetMoveSpeed(float move_speed) noexcept
+{
+	move_speed_ = move_speed;
+}
+
+void UCharacterStatComponent::SetSightRange(float sight_range) noexcept
+{
+	sight_range_ = sight_range;
 }
 
