a95bac63408f9ee5ce48a227da7e957b48314ea5
Sinil <rtd99062@gmail.com>
2024-09-27 22:36:31 +0900

    #3 - Add maximum data of each attributes. Add ID to set stats.
    IK_Proto_GameData - Add maximum limits labeled by -1 character ID.
    CharacterStatComponent - In Initialization, get maximum limits of each stats. Set minimum data between max and parameter when setters has called.

--- Source/Project_IK/Private/CharacterStatComponent.cpp
+++ Source/Project_IK/Private/CharacterStatComponent.cpp
@@ -10,11 +10,16 @@ See LICENSE file in the project root for full license information.
 
 #include "Project_IK/Public/CharacterStatComponent.h"
 
+#include "Math/UnrealMathUtility.h"
+
 #include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 #include "Project_IK/Public/IKGameInstance.h"
 
+
+
 // Sets default values
 UCharacterStatComponent::UCharacterStatComponent()
+	: character_id_(0)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryComponentTick.bCanEverTick = false;
@@ -28,7 +33,7 @@ void UCharacterStatComponent::InitializeComponent()
 
 	auto ik_game_instance = Cast<UIKGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
 
-	FCharacterData* character_data = ik_game_instance->GetCharacterData(0);
+	FCharacterData* character_data = ik_game_instance->GetCharacterData(character_id_);
 
 	ability_power_ = character_data->ability_power_;
 	attack_ = character_data->attack_;
@@ -39,6 +44,19 @@ void UCharacterStatComponent::InitializeComponent()
 	fire_range_ = character_data->fire_range_;
 	move_speed_ = character_data->move_speed_;
 	sight_range_ = character_data->sight_range_;
+
+
+	FCharacterData* max_stats = ik_game_instance->GetCharacterData(-1);
+
+	max_ability_power_ = max_stats->ability_power_;
+	max_attack_ = max_stats->attack_;
+	max_attack_speed_ = max_stats->attack_speed_;
+	max_hit_point_ = max_stats->hit_point_;
+	max_magazine_ = max_stats->magazine_;
+
+	max_fire_range_ = max_stats->fire_range_;
+	max_move_speed_ = max_stats->move_speed_;
+	max_sight_range_ = max_stats->sight_range_;
 }
 
 // Called when the game starts or when spawned
@@ -96,41 +114,41 @@ float UCharacterStatComponent::GetSightRange() noexcept
 
 void UCharacterStatComponent::SetAbilityPower(float ability_power) noexcept
 {
-	ability_power_ = ability_power;
+	ability_power_ = FMath::Min(ability_power, max_ability_power_);
 }
 
 void UCharacterStatComponent::SetAttack(float attack) noexcept
 {
-	attack_ = attack;
+	attack_ = FMath::Min(attack, max_attack_);
 }
 
 void UCharacterStatComponent::SetAttackSpeed(float attack_speed) noexcept
 {
-	attack_speed_ = attack_speed;
+	attack_speed_ = FMath::Min(attack_speed, max_attack_speed_);
 }
 
 void UCharacterStatComponent::SetHitPoint(float hit_point) noexcept
 {
-	hit_point_ = hit_point;
+	hit_point_ = FMath::Min(hit_point, max_hit_point_);
 }
 
 void UCharacterStatComponent::SetMagazine(float magazine) noexcept
 {
-	magazine_ = magazine;
+	magazine_ = FMath::Min(magazine, max_magazine_);
 }
 
 void UCharacterStatComponent::SetFireRange(float fire_range) noexcept
 {
-	fire_range_ = fire_range;
+	fire_range_ = FMath::Min(fire_range, max_fire_range_);
 }
 
 void UCharacterStatComponent::SetMoveSpeed(float move_speed) noexcept
 {
-	move_speed_ = move_speed;
+	move_speed_ = FMath::Min(move_speed, max_move_speed_);
 }
 
 void UCharacterStatComponent::SetSightRange(float sight_range) noexcept
 {
-	sight_range_ = sight_range;
+	sight_range_ = FMath::Min(sight_range, max_sight_range_);
 }
 
