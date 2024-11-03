e9cd38cd4217e3256b266fe3eea556ca97330ac4
Mook Kim <chunmook.kim97@gmail.com>
2024-11-03 13:25:47 +0900

    #35 - WIP

---
+++ Source/Project_IK/Private/FindNearestEnemy.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FindNearestEnemy.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Class.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
+
+UFindNearestEnemy::UFindNearestEnemy() 
+{
+	NodeName = "FindNearestEnemy";
+	Interval = 0.05f;
+	RandomDeviation = 0.f;
+
+	target_class_key_.AddClassFilter(this, GET_MEMBER_NAME_CHECKED(UFindNearestEnemy, BlackboardKey), AActor::StaticClass());
+	target_enemy_key_.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UFindNearestEnemy, BlackboardKey), AActor::StaticClass());
+}
+
+void UFindNearestEnemy::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	const UBlackboardComponent* blackboard = OwnerComp.GetBlackboardComponent();
+	AActor* owner_actor = OwnerComp.GetOwner();
+	UWorld* world = GetWorld();
+
+	if (blackboard) 
+	{
+		//TODO: Gunner역시 C++로 구현한 다음, Object를 Gunner로 Cast해야 한다.
+		UObject* target_enemy = blackboard->GetValueAsObject(target_enemy_key_.SelectedKeyName);
+		UClass* target_class = blackboard->GetValueAsClass(target_class_key_.SelectedKeyName);
+
+		//if (target_class && target_enemy) {
+		//	TArray<FOverlapResult> OverlapResults;
+		//	FCollisionQueryParams CollisionQueryParam(NAME_None, false, owner_actor);
+		//	//TODO: 여기다 class를 넣어야 함.
+		//	FCollisionObjectQueryParams query_params(ECollisionChannel::ECC_WorldStatic);
+		//	bool result = world->OverlapMultiByObjectType(
+		//		OverlapResults,
+		//		owner_actor->GetActorLocation(),
+		//		FQuat::Identity,
+		//		query_params,
+		//		FCollisionShape::MakeSphere(/*Gunner->sightRange*/0),
+		//		CollisionQueryParam
+		//	);
+		//}
+		//TODO: 가장 가까운 적을 다시 blackboard에 set해야 한다.
+	}
+}
---
+++ Source/Project_IK/Public/FindNearestEnemy.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "FindNearestEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_IK_API UFindNearestEnemy : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UFindNearestEnemy();
+	//TODO: PROPERTY달아야 함.
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
+	//TODO: PROPERTY달아야 함.
+	FBlackboardKeySelector target_class_key_;
+	FBlackboardKeySelector target_enemy_key_;
+};
