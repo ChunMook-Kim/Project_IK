422fad7512a2bcd8ba87525c7aca52753956c4ea
Sinil <rtd99062@gmail.com>
2024-11-03 23:52:58 +0900

    #37 - Created initial map files.
    IKMaps - Files contain logics about maps.

---
+++ Source/Project_IK/Private/IKMaps.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 11.03.2024
+Summary : Source file for Map system.
+					This file handles all actions about map.
+					For example: procedurally generation of map, getter&setter, ...
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+
+#include "IKMaps.h"
+
+UIKMaps::UIKMaps()
+	: Super::UObject()
+{
+}
+
+void UIKMaps::GenerateMaps(int32 row, int32 col)
+{
+
+}
