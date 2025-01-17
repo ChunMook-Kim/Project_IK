54ae0690257b8f48cb355f22251761fb412b7cde
Sinil <rtd99062@gmail.com>
2025-01-17 23:34:11 +0900

    #94 - Created slots for store and imagined store layouts.
    BP_StoreSlot - A Blueprint asset for store slot.
    BP_StoreWidget - Tested several widgets to imagine final view of store scene.
    StoreSlot - Created a new slot for store.

---
+++ Source/Project_IK/Private/UI/StoreSlot.cpp
@@ -0,0 +1,13 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 01.17.2024
+Summary : Source file for a slot UserWidget for Store.
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+
+#include "UI/StoreSlot.h"
+
---
+++ Source/Project_IK/Public/UI/StoreSlot.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 01.17.2024
+Summary : Header file for a slot UserWidget for Store.
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "StoreSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_IK_API UStoreSlot : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+
+	
+};
